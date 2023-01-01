24538c6b32f760dfb6fb05dc0e7fc06e3ee3fa7d
nifanfa <nifanfa@foxmail.com>
2023-01-01 14:47:55 +0800

    Use vmware player as debugger

--- run.bat
+++ run.bat
@@ -1,4 +1,13 @@
+@echo off
 nasm -fbin loader.asm -o loader.o
 copy /b loader.o+Debug\kernel.exe grub2\boot\kernel.bin
 mkisofs.exe -relaxed-filenames -J -R -o kernel.iso -b boot/grub/i386-pc/eltorito.img -no-emul-boot -boot-load-size 4 -boot-info-table grub2
-"C:\Program Files\qemu\qemu-system-x86_64.exe" -m 1024 -smp 2 -k en-gb -boot d -cdrom kernel.iso -d guest_errors -serial stdio -device AC97 -rtc base=localtime
+if exist "C:\Program Files (x86)\VMware\VMware Workstation\vmplayer.exe" (
+	"C:\Program Files (x86)\VMware\VMware Workstation\vmplayer.exe" "Solution2.vmx"
+) else if exist "C:\Program Files (x86)\VMware\VMware Player\vmplayer.exe" (
+	"C:\Program Files (x86)\VMware\VMware Player\vmplayer.exe" "Solution2.vmx"
+) else (
+	echo "Please install VMWare Player in order to Run Solution2!"
+	pause
+	exit
+)
